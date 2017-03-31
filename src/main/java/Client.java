c8ea30767a1f8e7f5b44db937cdfc314ee2f4eeb
Riley Watts <rileywatts.yes@gmail.com>
2017-03-31 09:38:09 -0700

    reinstantiate stylist methods/tests to fit client.java

---
+++ src/main/java/Client.java
@@ -0,0 +1,63 @@
+import org.sql2o.*;
+import java.util.List;
+
+public class Client {
+  private String name;
+  private String details;
+  private int id;
+
+  public Client(String name, String details) {
+    this.name = name;
+    this.details = details;
+  }
+
+  @Override
+  public boolean equals(Object otherClient) {
+    if(!(otherClient instanceof Client)) {
+      return false;
+    } else {
+      Client newClient = (Client) otherClient;
+      return this.getName().equals(newClient.getName()) &&
+             this.getId() == newClient.getId();
+    }
+  }
+
+  public String getName() {
+    return name;
+  }
+  public int getId() {
+    return id;
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO clients (name, details) VALUES (:name, :details)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("details", this.details)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Client> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM clients";
+      return con.createQuery(sql)
+      .executeAndFetch(Client.class);
+    }
+  }
+
+  public static Client find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM clients where id=:id";
+      Client stylist = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Client.class);
+      return stylist;
+    }
+  }
+
+
+
+}
---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,52 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ClientTest {
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM clients *;";
+      con.createQuery(sql).executeUpdate();
+    }
+  }
+
+  @Test
+  public void getName_returnsNameOfStylist_string() {
+    Client newClient = new Client("Frannie", "Always with the mints");
+    assertEquals(newClient.getName(), "Frannie");
+  }
+
+
+  @Test
+  public void equals_returnsTrueIfStringContentsAreTheSame() {
+    Client firstClient = new Client("Mel", "Smells like mothballs");
+    Client secondClient = new Client("Mel", "Smells like mothballs");
+    assertTrue(firstClient.equals(secondClient));
+  }
+
+  @Test
+  public void save_savesIntoDatabase_true() {
+    Client myClient = new Client("Mel", "Smells like mothballs");
+    myClient.save();
+    assertTrue(Client.all().get(0).equals(myClient));
+  }
+
+  @Test
+  public void find_findsClientWithMatchingId_secondClient() {
+    Client firstClient = new Client("Frannie", "Always with the mints");
+    Client secondClient = new Client("Mel", "Smells like mothballs");
+    firstClient.save();
+    secondClient.save();
+    assertEquals(Client.find(secondClient.getId()),secondClient);
+  }
+
+
+
+}
